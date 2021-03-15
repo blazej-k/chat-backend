c1c939e2a893628767d3bab8810cbbec12a92d12
blazej-k <blazejkania1@gmail.com>
2021-03-15 19:40:22 +0100

    add sign in endpoint

--- server.js
+++ server.js
@@ -16,26 +16,38 @@ const users = []
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
-// app.post('/saveUser', (req, res) => {
-//     console.log(req.body)
-//     const { login, password, sex } = req.body
-//     const model = new ChatModel({
-//         login, password, sex, active: true, date: new Date()
-//     })
-//     model.save(err => {
-//         if (err) {
-//             console.log(err)
-//         }
-//         else {
-//             res.send(model)  
-//         }
-//     })
-// })
+app.post('/saveUser', (req, res) => {
+    console.log(req.body)
+    const { login, password, sex } = req.body
+    const model = new ChatModel({
+        login, password, sex, active: true, date: new Date()
+    })
+    model.save(err => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.send(model)  
+        }
+    })
+})
+
+app.post('/signIn', async(req, res) => {
+    const {login, password } = req.body
+    const user = await ChatModel.findOne({login, password})
+    if(user){
+        res.send(user) 
+    }
+    else{
+        res.send('null')
+    } 
+})
 
 
 io.on('connection', socket => {
-    socket.on('setuser', ({ id }) => { 
-        users
+    socket.on('add user to listeners', login  => { 
+        users.push({id: socket.id, login})
+        console.log(users)
     })
     socket.on('test', ({ name }) => {
         messages.push(name)
@@ -43,7 +55,7 @@ io.on('connection', socket => {
     })
 })
 
-
+ 
 
 mongoose.connect("mongodb://localhost:27017/chatDB", {
     useUnifiedTopology: true,
